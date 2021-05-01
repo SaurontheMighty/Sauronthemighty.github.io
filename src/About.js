4053d146f4f6b1d6764551b43967da55e8f17e12
Ashish <yesashishs@gmail.com>
2021-05-01 13:10:02 -0400

    Updating for Deployment

--- src/About.js
+++ src/About.js
@@ -1,13 +1,9 @@
-import avatar from "./images/avatar.png";
-import github from "./images/githublogo.png";
-import linkedin from "./images/linkedinlogo.png";
-import hackathons from "./images/hacklogo.png";
 
 const About = () => {
     return (
         <section className="about-card">
             <p className="section-header">About Me</p>
-            <img src={avatar} className="avatar" alt="Avatar Image" />
+            <img src="images/avatar.png" className="avatar" alt="Avatar Image" />
             <section className="about-details">
                 <p className="about-details-header">Passionate about writing Software<br /> and Building Projects.</p>
                 <ul className="small-tags">
@@ -24,12 +20,12 @@ const About = () => {
             </section>
             <section className="links">
                 <button className="about-button" onClick={() => window.open('https://github.com/SaurontheMighty/','_blank')}><img
-                    className='abt-btn-img' src={github} alt="GitHub Logo" /></button>
+                    className='abt-btn-img' src="images/githublogo.png" alt="GitHub Logo" /></button>
                 <button className="about-button"
                     onClick={() => window.open('https://www.linkedin.com/in/ashish-selvaraj/','_blank')}><img
-                    className='abt-btn-img' src={linkedin} alt="LinkedIn Logo" /></button>
+                    className='abt-btn-img' src="images/linkedinlogo.png" alt="LinkedIn Logo" /></button>
                 <button className="about-button" onClick={() => window.open('https://devpost.com/yesashishs','_blank')}><img
-                    className='abt-btn-img' src={hackathons} alt="Hackathons" /></button>
+                    className='abt-btn-img' src="images/hacklogo.png" alt="Hackathons" /></button>
             </section>
       </section> 
     );
