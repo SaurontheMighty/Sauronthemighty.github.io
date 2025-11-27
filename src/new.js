1ac22b32ad4dde9a85ad20673101b4a436b7aea3
Ashish Selvaraj <selvaraj.ashish@gmail.com>
2025-11-26 22:18:54 -0500

    update

--- src/new.js
+++ src/new.js
@@ -13,13 +13,13 @@ function Simple() {
                     Your browser does not support the video tag.
                 </video><br></br><br></br>
                 <b>Present</b>
-                <p>After 5 years of constant work or study, I'm on a 6 month break before I start full-time in a bid to have fun with software again.</p>
+                <p>After 5 years of non-stop work or study, I'm taking a break to have fun with software again.</p>
                 <ul>
                     <li><a href="https://appsidekit.com">Sidekit</a>: A backend for your app in one package with lightweight analytics, feature flags, version gating and more.</li>
                     <li><a href="https://posturing.app">Posturing</a>: Posture reminders for Mac that show you a live feed of your posture every X minutes</li>
                     <li><a href="https://notevil.io">Not Evil Sudoku</a>: More sudoku updates!</li>
                 </ul>
-                <p>You can find my resume <a href="/resume.pdf">here</a>. I open my <a href="https://twitter.com/selvarajashish">x (twitter)</a> every few months.</p>
+                <p>You can find my resume <a href="/resume.pdf">here</a>. I open my <a href="https://twitter.com/asheeeeeshh">x (twitter)</a> every few months.</p>
                 <b>Past</b>
                 <p>Graduated with a double degree in Computer Science and Business from the University of Waterloo in August 2025 🎉 after 5 years and 5 internships (the Waterloo classic).</p>
                 <p>Professionally, I like working on distributed systems, most recently I worked on building <a href="https://docs.databricks.com/aws/en/dlt-ref/dlt-sql-ref-create-view">persisted views</a> for Delta Live Tables, <strong>Databricks</strong>' declarative ETL pipeline product.</p>
